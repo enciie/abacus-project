91f2c96b1bf2fab9e14c6a2c16fca17b13a7b782
Nancy Jo <enciie@gmail.com>
2018-08-21 23:41:09 +0000

    create groups table

--- db/migrate/20180821233636_create_groups.rb
+++ db/migrate/20180821233636_create_groups.rb
@@ -1,7 +1,8 @@
 class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
-
+      t.string :name
+      t.integer :user_id
       t.timestamps null: false
     end
   end
