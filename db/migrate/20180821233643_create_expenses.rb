ac9070719b09eaee6b5a83fa4b8045c8fa54ada1
Nancy Jo <enciie@gmail.com>
2018-08-21 23:43:10 +0000

    create expenses table

--- db/migrate/20180821233643_create_expenses.rb
+++ db/migrate/20180821233643_create_expenses.rb
@@ -1,7 +1,10 @@
 class CreateExpenses < ActiveRecord::Migration
   def change
     create_table :expenses do |t|
-
+      t.string :name
+      t.decimal :price
+      t.integer :user_id
+      t.integer :group_id
       t.timestamps null: false
     end
   end
