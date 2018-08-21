a6349929b682722fa4706938f24c02881e250727
Nancy Jo <enciie@gmail.com>
2018-08-21 23:47:12 +0000

    create associations between models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,4 @@
 class Expense < ActiveRecord::Base
+  belongs_to :group
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :groups
+  has_many :expenses, :through => :groups
+
+  has_secure_password
+  validates_presence_of :username, :email, :password
+  validates_uniqueness_of :username, :email
 end
