a5302fe1f0a3600ebc48f1afe6a859ceb06e70f9
Nancy Jo <enciie@gmail.com>
2018-08-22 00:55:19 +0000

    setup view files and create sign and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,41 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :sessions_secret, "abacus-secret"
   end
 
   get "/" do
-    erb :welcome
+    erb :index
+  end
+
+  get "/signup" do
+    erb :"/users/new"
+  end
+
+  post "/signup" do # create a new user
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/groups'
+    end
+  end
+
+  get "/login" do
+    erb :"/users/index"
+  end
+
+  post "/login" do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/groups'
+    else
+      redirect to '/signup'
+    end
   end
 
 end
